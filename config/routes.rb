bfcb25fd7b81030780a05adf6117955fc237bdb1
Jon Caneiro <joncaneiro@gmail.com>
2019-11-01 17:10:20 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'about', to: 'static#about'
+
+# "get" -> is the HTTP verb.
+
+# 'about' -> is the URL path that will have a mapped route.
+
+# 'to: static#about' -> tells Rails to pass the route through 
+#  the "static" controllers "about" action. 
+
+# "action" is method within a controller
+
+# So, in the 'StaticController' there will be a method
+# calling 'about' which gets called on when a user goes
+# to '/about'
+
 end
+
